39813dbcefa4ee61b5f760ab8e07c6f645990dca
Oscar Zuñiga Lara <30669700+OscarZunigaLara@users.noreply.github.com>
2021-08-31 21:00:51 -0500

    8/31
    8/31

---
+++ lab/reactApp/reactapp/src/VideojueogsApp.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+import ReactDom from 'react-dom';
+const titulo = 'hola'
+const VideojueogsApp = () => {
+    return (
+        <Fragment>
+            <h1>
+                Titulo de pagina: {titulo}
+            </h1>
+            <h2>
+                BIEN VENIDO
+            </h2>
+        </Fragment>
+    )
+}
+
+export default VideojueogsApp;
